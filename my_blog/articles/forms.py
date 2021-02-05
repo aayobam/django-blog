8ed0eca2e59e7516a6db863361d0dc7309e2e1b5
Oluwa Abayomi <aayobam@gmail.com>
2021-02-05 19:23:31 +0800

    Update forms.py

--- my_blog/articles/forms.py
+++ my_blog/articles/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from .models import Article, Comment
 
 
+# This is an instance of the article creation model class.This returns article creation page to the registered users and authorised users on the blog page .
 class CreatArticle(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'body', 'slug', 'thumb']
+        
 
-
+# This is an instance of the Comments creation model class. This returns comments form page to the users on the blog page.
 class CreateComment(forms.ModelForm):
     class Meta:
         model = Comment
