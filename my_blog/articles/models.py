f32743fff0f464b5fa0d515226b368d6ccc5bdad
Oluwa Abayomi <aayobam@gmail.com>
2021-02-05 18:54:42 +0800

    Update models.py

--- my_blog/articles/models.py
+++ my_blog/articles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# This creates tables for the articles data fields and data at the admin area.
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
@@ -14,10 +14,11 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+    # This returns partial parts of the article body at the articles list page
     def snippet(self):
         return self.body[:50] + "..."
 
-
+# This creates tables for comments and their associated articles at the admin area.
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete= models.CASCADE, related_name='comments')
     name = models.CharField(max_length=50)
@@ -27,9 +28,10 @@ class Comment(models.Model):
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
 
+    # This returns comments according to the date they are been made
     class Meta:
         ordering = ('created_on',)
 
-
+    # This returns comment infirmation by visitors or users
     def __str__(self):
         return "comment by {} on {}".format(self.name, self.created_on)
