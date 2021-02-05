359619047ccf912cd0cbfdcd81e758f36c48f176
Oluwa Abayomi <aayobam@gmail.com>
2021-02-05 18:48:15 +0800

    Update admin.py

--- my_blog/articles/admin.py
+++ my_blog/articles/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Article, Comment
 
 
+# This registers the Article table at the Admin area
 @admin.register(Article)
 class AdminArticle(admin.ModelAdmin):
     list_display = ("title", "body", "thumb", "author", "date")
@@ -9,7 +10,7 @@ class AdminArticle(admin.ModelAdmin):
     list_filter = ("author", "date")
     
 
-
+# This registers the Comment Table at the Admin area
 @admin.register(Comment)
 class AdminComment(admin.ModelAdmin):
     list_display = ('name', 'email', 'article','comment_text', 'created_on', 'active')
@@ -17,6 +18,6 @@ class AdminComment(admin.ModelAdmin):
     search_fields = ('name', 'email', 'comment_text')
     actions = ["approve_comments"]
 
-
+    # This function display active comments on a article by users which has been approved as active by the Administrator.
     def approve_comments(self, request, queryset):
         return queryset.update(active=True)
